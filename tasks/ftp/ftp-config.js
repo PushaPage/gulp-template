d93e81f32a7ab1ea6761a11786b7a215fa701e91
Vadym <vadim.stebakov.94@gmail.com>
2019-08-09 07:40:16 +0300

    fix deploy config

--- tasks/ftp/ftp-config.js
+++ tasks/ftp/ftp-config.js
@@ -6,9 +6,9 @@ const $ = require('gulp-load-plugins')();
 module.exports = function() {
 	return {
 		connect: ftp.create({
-			host: 's106.aiwebhost.com',
-			user: 'airstudi',
-			password: 'uev37U77pT',
+			host: 'host',
+			user: 'user',
+			password: 'password',
 			parallel: 10,
 			reload: true,
 			maxConnections: 15,
